8b9e173ea7ffeabc30f6f80d38e5e1198c940ac3
starranuk <starran@live.co.uk>
2022-01-28 16:30:12 +0000

    html pages added to templates folder

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "0.0.0.0."),
+        host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
-        
